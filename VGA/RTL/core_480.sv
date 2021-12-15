a1f3bcb39466b020613f9779cba823ba48d00481
Hugo Medeiros <hugo.medeiros@embedded.ufcg.edu.br>
2021-12-14 22:24:14 -0300

    Fix in Draw_enable signal

--- VGA/RTL/core_480.sv
+++ VGA/RTL/core_480.sv
@@ -8,17 +8,17 @@ module core_480(
   output logic  [9:0] o_Sy  
 ); 
   
-  localparam H_LENGTH			=800;
-  localparam H_BACK_PORCH		=48;// 48=0x30
+  localparam H_LENGTH		=800;
+  localparam H_BACK_PORCH	=48;// 48=0x30
   localparam H_ACTIVE_REGION	=H_BACK_PORCH+640;//688 = 0x2B0 
-  localparam H_FRONT_PORCH		=H_ACTIVE_REGION+16;// 704 = 0x2C0
-  localparam H_SYNC				=H_FRONT_PORCH+96;// 800 = 0x320
+  localparam H_FRONT_PORCH	=H_ACTIVE_REGION+16;// 704 = 0x2C0
+  localparam H_SYNC		=H_FRONT_PORCH+96;// 800 = 0x320
   
-  localparam V_LENGTH			=525;
-  localparam V_BACK_PORCH		=33;// 33 = 0x21
+  localparam V_LENGTH		=525;
+  localparam V_BACK_PORCH	=33;// 33 = 0x21
   localparam V_ACTIVE_REGION	=V_BACK_PORCH + 480;// 513 = 0x201
-  localparam V_FRONT_PORCH		=V_ACTIVE_REGION+10;// 523 = 0x20B 
-  localparam V_SYNC				=V_FRONT_PORCH+2;// 525 = 0x20D
+  localparam V_FRONT_PORCH	=V_ACTIVE_REGION+10;// 523 = 0x20B 
+  localparam V_SYNC		=V_FRONT_PORCH+2;// 525 = 0x20D
   
   logic x_active;
   logic y_active;
@@ -40,15 +40,15 @@ module core_480(
 // SYNC logic
     always_comb begin
       // hsync = 1 when 0x2C0 <= o_Sx < H_SYNC
-      o_hsync = ~((o_Sx>= H_FRONT_PORCH-1  && o_Sx<H_SYNC-1)? 1'b1: 1'b0);
-      o_vsync = ~((o_Sy>=V_FRONT_PORCH-1 && o_Sy<V_SYNC-1)? 1'b1: 1'b0);     
+      o_hsync = ~((o_Sx> H_FRONT_PORCH-1  && o_Sx<=H_SYNC-1)? 1'b1: 1'b0);
+      o_vsync = ~((o_Sy>V_FRONT_PORCH-1 && o_Sy<=V_SYNC-1)? 1'b1: 1'b0);     
     end
     
 // Draw-enable logic (Active Region)
     always_comb begin
       x_active = (o_Sx>=H_BACK_PORCH-1&&o_Sx<H_ACTIVE_REGION-1)? 1'b1: 1'b0;
-      y_active = (o_Sy>=H_BACK_PORCH-1&&o_Sy<V_ACTIVE_REGION-1)? 1'b1: 1'b0;
-      o_de = x_active && y_active; 
+      y_active = (o_Sy>=V_BACK_PORCH-1&&o_Sy<V_ACTIVE_REGION-1)? 1'b1: 1'b0;
+      o_de = x_active & y_active; 
     end
     
 
